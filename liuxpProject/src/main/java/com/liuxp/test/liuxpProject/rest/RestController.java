c4b40deee1848b76c2093cfe2fad5fa36afa7fe7
liuxiangp <lxp935342295@163.com>
2017-10-25 14:12:16 +0800

    去掉log4j

--- liuxpProject/src/main/java/com/liuxp/test/liuxpProject/rest/RestController.java
+++ liuxpProject/src/main/java/com/liuxp/test/liuxpProject/rest/RestController.java
@@ -1,11 +1,9 @@
 package com.liuxp.test.liuxpProject.rest;
 
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.web.bind.annotation.RestController
-@Log4j2
 public class RestController {
     @RequestMapping(value =  "test")
     public String testRest(){
