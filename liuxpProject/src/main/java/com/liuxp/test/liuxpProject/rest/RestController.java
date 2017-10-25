b363d4e1d3c49e759708eac9cf1d7c3a427e2c26
liuxiangp <lxp935342295@163.com>
2017-10-25 16:21:14 +0800

    log4j2

--- liuxpProject/src/main/java/com/liuxp/test/liuxpProject/rest/RestController.java
+++ liuxpProject/src/main/java/com/liuxp/test/liuxpProject/rest/RestController.java
@@ -1,13 +1,16 @@
 package com.liuxp.test.liuxpProject.rest;
 
-
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.log4j.Log4j2;
+
+
 @org.springframework.web.bind.annotation.RestController
+@Log4j2
 public class RestController {
     @RequestMapping(value =  "test")
     public String testRest(){
-        System.out.println("test");
+        log.info("test");
         return "success";
     }
 }
